c9e658eff106ddb9b4a4815c74df7cbed7c8ffec
Roman <roman.songoku.army@gmail.com>
2023-04-13 10:08:39 +0700

    13.04 доделал функции в js и добавил изменение цве

--- JS/New.js
+++ JS/New.js
@@ -1,27 +1,39 @@
-const btn1 = document.getElementById('btn1');
-const list = document.getElementById('listElements');
-const txt = document.getElementById("input1");
+const todo = {
+    action(e){
+        const target = e.target;
+        if(target.classList.contains('todo_add')){
+            this.add();
+            this.save();
+        }
+        else if(target.classList.contains('todo_remove')){
+            target.parentNode.remove(target);
+            localStorage.setItem('todo', document.querySelector('.todo_items').innerHTML);
+        }
+    },
+    add() {
+        const elemText = document.querySelector('.input1');
+        if (elemText.disabled || !elemText.value.length) {
+            return;
+        }
+        document.querySelector('.todo_items').insertAdjacentHTML('beforeend', this.create(elemText.value));
+        elemText.value = '';
+    },
+    create(text){
+        return `<li class=todo_item>
+        ${text}
+        <button class="todo_remove">-</button>`;
+    },
+    init(){
+        const fromStorage = localStorage.getItem('todo');
+        if(fromStorage){
+            document.querySelector('.todo_items').innerHTML = fromStorage;
+        }
 
-
-
-btn1.addEventListener('click', clicker());
-
-
-
-function clicker()
-{
-    console.log(txt);
-    let newEl = document.createElement('li');
-
-    
-    newEl.appendChild();
-    list.appendChild(newEl);
-    if(false)
-    {
-        newText = document.createTextNode(txt);
-        newEl.appendChild(newText);
-        list.appendChild(newEl);
-        console.log('Done!');
+        document.addEventListener('click', this.action.bind(this));
+    },
+    save(){
+        localStorage.setItem('todo', document.querySelector('.todo_items').innerHTML);
     }
-    
 };
+
+todo.init();
